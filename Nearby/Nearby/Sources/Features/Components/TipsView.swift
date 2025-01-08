ca2446da14cdb3223b026413045b5c1a233db45c
Alan Pitanga <alansantosbahia@gmail.com>
2025-01-07 21:58:50 -0300

    Create TipsView

---
+++ Nearby/Nearby/Sources/Features/Components/TipsView.swift
@@ -0,0 +1,38 @@
+//
+//  TipsView.swift
+//  Nearby
+//
+//  Created by Alan Pitanga on 07/01/25.
+//
+
+import Foundation
+import UIKit
+
+public class TipsView: UIView {
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(imageView: UIImageView, title: String, description: String) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

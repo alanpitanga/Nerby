7e7d50bd84628aceb020307616a4ccd8c7d52005
Alan Pitanga <alansantosbahia@gmail.com>
2025-01-13 22:48:42 -0300

    font adjustments and new component creation

--- Nearby/Nearby/Sources/Features/Scenes/WelcomeView/WelcomeView.swift
+++ Nearby/Nearby/Sources/Features/Scenes/WelcomeView/WelcomeView.swift
@@ -20,6 +20,8 @@ class WelcomeView: UIView {
     private let welcomeLabel: UILabel = {
         let label = UILabel()
         label.text = "Boas vindas ao Nearby!"
+        label.font = Typography.titleXL
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
@@ -28,8 +30,18 @@ class WelcomeView: UIView {
     private let descriptionLabel: UILabel = {
         let label = UILabel()
         label.text = "Tenha cupons de vantagem para usar em seus estabelecimentos favoritos."
+        label.font = Typography.titleMD
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
 
+        return label
+    }()
+    
+    private let subTextForTips: UILabel = {
+        let label = UILabel()
+        label.text = "Veja como funciona:"
+        label.font = Typography.titleMD
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -47,6 +59,7 @@ class WelcomeView: UIView {
         button.setTitle("Começar", for: .normal)
         button.backgroundColor = Colors.greenBase
         button.setTitleColor(Colors.gray100, for: .normal)
+        button.titleLabel?.font = Typography.action
         button.layer.cornerRadius = 8
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -68,6 +81,7 @@ class WelcomeView: UIView {
         self.addSubview(logoImageView)
         self.addSubview(welcomeLabel)
         self.addSubview(descriptionLabel)
+        self.addSubview(subTextForTips)
         self.addSubview(tipsStackView)
         self.addSubview(startButton)
         
@@ -82,12 +96,17 @@ class WelcomeView: UIView {
             logoImageView.heightAnchor.constraint(equalToConstant: 48),
             
             welcomeLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 24),
-            welcomeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            welcomeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             
             descriptionLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8),
             descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
             
-            tipsStackView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 24),
+            subTextForTips.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 24),
+            subTextForTips.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+
+            
+            tipsStackView.topAnchor.constraint(equalTo: subTextForTips.bottomAnchor, constant: 24),
             tipsStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
             tipsStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
             

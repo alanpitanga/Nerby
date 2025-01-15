c5c34f6a3be9ad4dcd5960c5f28987e69f23abe4
Alan Pitanga <alansantosbahia@gmail.com>
2025-01-15 00:15:29 -0300

    Create HomeView

--- Nearby/Nearby/Sources/Features/Scenes/HomeView/HomeView.swift
+++ Nearby/Nearby/Sources/Features/Scenes/HomeView/HomeView.swift
@@ -6,3 +6,104 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+
+class HomeView: UIView {
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    private let filterScrollView: UIScrollView = {
+       let scrollView = UIScrollView()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isUserInteractionEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Colors.gray100
+        view.layer.cornerRadius = 16
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let dragIndicatorView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 3
+        view.backgroundColor = Colors.gray300
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let descriptionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Explore locais perto de você"
+        label.font = Typography.textMD
+        label.textColor = Colors.gray600
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private var containerTopConstraints: NSLayoutConstraint!
+    
+    private func setupUI() {
+        addSubview(mapView)
+        addSubview(filterScrollView)
+        addSubview(containerView)
+        containerView.addSubview(dragIndicatorView)
+        containerView.addSubview(descriptionLabel)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mapView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.65),
+            
+            filterScrollView.topAnchor.constraint(equalTo: topAnchor, constant: 48),
+            filterScrollView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            filterScrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            filterScrollView.heightAnchor.constraint(equalToConstant: 86),
+            
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        
+        ])
+        
+        containerTopConstraints = containerView.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -16)
+        containerTopConstraints.isActive = true
+        
+        NSLayoutConstraint.activate([
+            dragIndicatorView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
+            dragIndicatorView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            dragIndicatorView.widthAnchor.constraint(equalToConstant: 80),
+            dragIndicatorView.heightAnchor.constraint(equalToConstant: 4),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: dragIndicatorView.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 24),
+            descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -24),
+
+
+            
+        ])
+    }
+}
